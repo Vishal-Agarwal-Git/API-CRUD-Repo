d50e8732ba0a4412c080e8a623a85427d2e80aa6
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-24 23:48:28 +0530

    ̣

---
+++ API-CRUD-Repo/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using API_CRUD_Repo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CRUD_Repo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
